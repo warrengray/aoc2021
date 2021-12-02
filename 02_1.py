7067eb9b53145ee2b781689eaac466b59d5fa26d
Warren Gray <warren.gray@arcticwolf.com>
2021-12-02 09:05:34 -0500

    Day 2

---
+++ 02_1.py
@@ -0,0 +1,32 @@
+from util import readlines
+
+EXAMPLE = """
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+"""
+
+EXAMPLE = EXAMPLE.strip().split("\n")
+
+if __name__ == '__main__':
+  def main():
+    # moves = EXAMPLE
+    moves = readlines('02.input.txt')
+    horizontal, depth = 0, 0
+    for move in moves:
+      command, value = move.split(" ")
+      value = int(value)
+      if command == 'forward':
+        horizontal += value
+      elif command == 'up':
+        depth -= value
+      elif command == 'down':
+        depth += value
+
+    return horizontal * depth
+
+
+  print(main())
